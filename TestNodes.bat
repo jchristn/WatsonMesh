8a43400bf06cff49087b6ecfcfa7fb7a5135fb8b
Joel Christner <joel.christner@gmail.com>
2023-01-11 19:13:43 -0800

    Code repo restructure

--- TestNodes.bat
+++ TestNodes.bat
@@ -1,4 +1,4 @@
-@start Test\bin\debug\net461\Test.exe -ipport=127.0.0.1:8000 -peers=127.0.0.1:8001,127.0.0.1:8002
-@start Test\bin\debug\net461\Test.exe -ipport=127.0.0.1:8001 -peers=127.0.0.1:8000,127.0.0.1:8002
-@start Test\bin\debug\net461\Test.exe -ipport=127.0.0.1:8002 -peers=127.0.0.1:8000,127.0.0.1:8001
+@start src\Test\bin\debug\net461\Test.exe -ipport=127.0.0.1:8000 -peers=127.0.0.1:8001,127.0.0.1:8002
+@start src\Test\bin\debug\net461\Test.exe -ipport=127.0.0.1:8001 -peers=127.0.0.1:8000,127.0.0.1:8002
+@start src\Test\bin\debug\net461\Test.exe -ipport=127.0.0.1:8002 -peers=127.0.0.1:8000,127.0.0.1:8001
 
---
+++ src/WatsonMesh/DefaultSerializationHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WatsonMesh
+{
+    /// <summary>
+    /// Default serialization helper.
+    /// </summary>
+    public class DefaultSerializationHelper : ISerializationHelper
+    {
+        #region Public-Members
+
+        #endregion
+
+        #region Private-Members
+
+        private bool _Pretty = false;
+
+        #endregion
+
+        #region Constructors-and-Factories
+
+        #endregion
+
+        #region Public-Methods
+
+        /// <summary>
+        /// Deserialize JSON to an instance.
+        /// </summary>
+        /// <typeparam name="T">Type.</typeparam>
+        /// <param name="json">JSON string.</param>
+        /// <returns>Instance.</returns>
+        public T DeserializeJson<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+
+        /// <summary>
+        /// Serialize object to JSON.
+        /// </summary>
+        /// <param name="obj">Object.</param>
+        /// <param name="pretty">Pretty print.</param>
+        /// <returns>JSON.</returns>
+        public string SerializeJson(object obj, bool pretty = true)
+        {
+            if (obj == null) return null;
+
+            if (!pretty)
+            {
+                return JsonSerializer.Serialize(obj);
+            }
+            else
+            {
+                return JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
+            }
+        }
+
+        /// <summary>
+        /// Instantiation method to support fixups for various environments, e.g. Unity.
+        /// </summary>
+        public void InstantiateConverter()
+        {
+            try
+            {
+                Activator.CreateInstance<JsonStringEnumConverter>();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region Private-Methods
+
+        #endregion
+    }
+}
